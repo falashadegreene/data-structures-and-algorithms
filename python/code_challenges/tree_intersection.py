126853d33fb7e1747e69bc5321743635c68eb235
Falashade Greene <falashadegreene@gmail.com>
2022-09-30 02:06:52 -0400

    finished code challenge and added whiteboard to readme

--- python/code_challenges/tree_intersection.py
+++ python/code_challenges/tree_intersection.py
@@ -1,5 +1,15 @@
 from data_structures.binary_tree import BinaryTree
 
+def tree_intersection(tree_a, tree_b):
+    same_nums = set()
+    tree_a_keys = {}
+    tree_a_values = BinaryTree.pre_order(tree_a)
+    tree_b_values = BinaryTree.pre_order(tree_b)
 
-def tree_intersection():
-    pass
+    for a_val in tree_a_values:
+        tree_a_keys[a_val] = None
+    for b_val in tree_b_values:
+        if b_val in tree_a_keys:
+            same_nums.add(b_val)
+
+    return list(same_nums)
